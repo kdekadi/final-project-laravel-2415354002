62def447a684c8fdbb73b633e4833e62c7611be4
kdekadi <ikadekadidarmawann@gmail.com>
2026-05-22 13:38:16 +0800

    Create subscriptions models

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscription extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'service_id',
+        'start_date',
+        'end_date',
+        'status'
+    ];
+
+    
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+   
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
 }
