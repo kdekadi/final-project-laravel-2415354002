9e67646b953c71249aed8ed8ae8d934b29cdeae2
kdekadi <ikadekadidarmawann@gmail.com>
2026-05-22 11:10:25 +0800

    Create customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'name',
+        'email',
+        'phone',
+        'address',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => 'boolean',
+        ];
+    }
 }
